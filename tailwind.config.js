41dd54c10121990fcb9aa101745359f947625692
KkutaySarii <h.kutay.1@gmail.com>
2023-03-30 17:14:59 +0300

    project updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#A435F0",
+      },
+    },
   },
   plugins: [],
 };
