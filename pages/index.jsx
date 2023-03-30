f9030aa6f71ba791e6cd69f75a71f0e61732f59b
KkutaySarii <h.kutay.1@gmail.com>
2023-03-30 17:26:15 +0300

    home page installed

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
+import Home from "./home"
 
-export default function Home() {
+export default function Index() {
   return (
     <>
       <Head>
@@ -9,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-center h-screen bg-primary">Food Ordering</h1>
+      <Home/>
     </>
   );
 }
