fe0808212e9fc8c563f2741f6c46f02e0dfa53c3
KkutaySarii <h.kutay.1@gmail.com>
2023-04-03 20:26:43 +0300

    primary-secondary color added and font added

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const index = () => {
   return (
-    <div>
+    <div className="text-primary font-dancing text-center text-3xl">
       Home Page
     </div>
-  )
-}
+  );
+};
 
 export default index;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#A435F0",
+        primary: "#ffbe33",
+        secondary: "#222831",
+      },
+      fontFamily: {
+        dancing: ["Dancing Script", "cursive"],
       },
     },
   },
