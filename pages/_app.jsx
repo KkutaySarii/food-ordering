29c07dc3cbd480edd68759e6a04f0972d8a7ba9c
KkutaySarii <h.kutay.1@gmail.com>
2023-06-13 16:19:50 +0300

    route change progress bar

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,22 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "../styles/globals.css";
-import "react-toastify/dist/ReactToastify.css";
+import { Router, useRouter } from "next/router";
 
 import { Provider } from "react-redux";
 import { SessionProvider } from "next-auth/react";
 import { ToastContainer } from "react-toastify";
-import axios from "axios";
+import nProgress from "nprogress";
 
 import { store } from "@/redux/store";
 import Layout from "@/components/Layout";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
+import "nprogress/nprogress.css";
+
+Router.events.on("routeChangeStart", () => nProgress.start());
+Router.events.on("routeChangeComplete", () => nProgress.done());
+Router.events.on("routeChangeError", () => nProgress.done());
 
 export default function App({
   Component,
